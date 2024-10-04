958a9ef85fd63c5fe1f4bb0a1b3dcfcaa54c935d
ParthaseeU <muhammad.parthasee1@umail.uom.ac.mu>
2024-10-04 17:22:49 +0400

    live server

--- source/AdminPage/subjectChange.php
+++ source/AdminPage/subjectChange.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-    <?php
-        require_once '../connect.php';
-    ?>
-  
+  <?php
+    require_once '../connect.php';
+    session_start();
+
+    if(iss)
+  ?>
+
 </body>
 </html>
